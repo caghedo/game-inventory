31a236ba54012b752dcbe7c81465060f5553505c
caghedo <caghedo13@gmail.com>
2022-07-01 14:04:45 +0000

    even MORE tweaks

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db = SQLAlchemy(app)
 
